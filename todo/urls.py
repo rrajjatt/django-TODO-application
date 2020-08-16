491915157558246b03015a83cc36118dc806c68b
rajat <goelrajat22@gmail.com>
2020-08-16 16:51:04 +0530

    A complete TODO application

---
+++ todo/urls.py
@@ -0,0 +1,36 @@
+"""todo URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from todoapp import views
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path("signup/",views.signupuser,name="signupuser"),
+    path('logout/',views.logoutuser,name='logoutuser'),
+    path('',views.home,name='home'),
+    path('login/',views.loginuser,name='loginuser'),
+
+    path('current/',views.currenttodo,name='currenttodo'),
+    path('create/',views.createtodo,name='createtodo'),
+    path('todo/<int:pk>/',views.viewtodo,name='viewtodo'),
+    path('todo/<int:pk>/complete',views.completetodo,name='completetodo'),
+    path('todo/<int:pk>/delete',views.deletetodo,name="deletetodo"),
+    path('completed/',views.completedtodo,name='completedtodo'),
+]
